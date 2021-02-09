ad5595263ab146bc964bbfc45c8e7c82aeb2f6b6
Anniina Jussila <nea.anniina@hotmail.com>
2021-02-09 15:55:44 +0000

    Finally database is working

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask import render_template
+from sqlalchemy import create_engine
+import os
 
 app = Flask(__name__)
 
+DATABASE_URL = os.environ['DATABASE_URL']
+
+db = create_engine(DATABASE_URL)
+
 @app.route('/')
 def index():
-    return "Hello dear heroku user!"
+    results = db.execute('SELECT name FROM city')      
+    return render_template('cities.html', cities=results)
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
